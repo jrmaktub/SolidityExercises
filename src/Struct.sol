74d9b86e122ac06993365f32bde40e822f7c0f04
jramirez09 <jramirez12@luc.edu>
2024-08-03 15:24:26 -0600

    delegate

--- src/Struct.sol
+++ src/Struct.sol
@@ -3,32 +3,25 @@ pragma solidity ^0.8.24;
 
 contract Struct { 
 
-    struct Todo {
-        string text;
-        bool completed;
-    }
+   struct Todo {
+    string text;
+    bool completed;
+   }
 
-    //after struct, always create Array
-    Todo[] public todos;
+    //array always
+   Todo[] public todos;
 
-    function create(string calldata _text) public {
-        todos.push(Todo(_text, false));
+   function create(string calldata _text) public {
+        todos.push(Todo(_text,false));
 
         Todo memory todo;
         todo.text = _text;
-    }
+   }
 
-    function get(uint256 _index) public view returns (string memory, bool completed) 
-    {
-        Todo storage todo = todos[_index];
-        return(todo.text, todo.completed);
-    }
-
-    function updateText(uint256 _index, string calldata _text) public {
+   function Update(uint256 _index ,string calldata _text) public {
         Todo storage todo = todos[_index];
         todo.text = _text;
-    }
-
+   }
   
 
 }
